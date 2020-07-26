cb8b1968ec66c8233190abf60ad707a02319a658
adamklosowicz <adam.klosowicz@gmail.com>
2020-07-26 23:01:21 +0200

    Code review

--- procedures/deleteLanguagesFromCountry.sql
+++ procedures/deleteLanguagesFromCountry.sql
@@ -18,13 +18,15 @@ BEGIN
 	BEGIN
 		SET is_success = 0;
 	END;
-    
+
 	SET all_languages_count = (SELECT count(*) FROM language WHERE country_id=operated_country_id);
 	SET languages_selected_count = get_char_occurance(lang_names_to_delete, ';')+1;
 
-	IF all_languages_count - languages_selected_count > 0 THEN
+	IF all_languages_count - languages_selected_count < 1 THEN
+		SET is_success = -1;
+	ELSE
 		SET languages_decorated = decorate_string_list(lang_names_to_delete, ';', ',', "'");
-	
+
 		SET tmp_where = CONCAT('WHERE country_id=', operated_country_id, ' AND `name` IN (', languages_decorated, ')');
 		SET @tmp_query = CONCAT('SELECT count(*) INTO @verified_languages_count FROM language ', tmp_where);
 		PREPARE select_stmt FROM @tmp_query;
@@ -35,7 +37,7 @@ BEGIN
 			SET is_success = -2;
 		ELSE
 			DEALLOCATE PREPARE select_stmt;
-	
+
 			SET @tmp_query = CONCAT('DELETE FROM language ', tmp_where);
 			SET SQL_SAFE_UPDATES = 0;
 			PREPARE delete_stmt FROM @tmp_query;
@@ -44,8 +46,6 @@ BEGIN
 			SET SQL_SAFE_UPDATES = 1;
 			SET is_success = 1;
 		END IF;
-	ELSE
-		SET is_success = -1;
 	END IF;
 	SELECT @is_success;
 END$$
