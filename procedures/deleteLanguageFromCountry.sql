0d2d465aa7052239fbbf874d630e094edb5bfc4a
adamklosowicz <adam.klosowicz@gmail.com>
2020-07-27 19:29:41 +0200

    Removed redundant transaction

--- procedures/deleteLanguageFromCountry.sql
+++ procedures/deleteLanguageFromCountry.sql
@@ -22,12 +22,10 @@ BEGIN
 		WHEN languages_count = 1 THEN
 			SET is_success = -1;
 		ELSE
-			START TRANSACTION;
 			SET SQL_SAFE_UPDATES = 0;
 			DELETE FROM language WHERE country_id=operated_country_id AND name=lang_name_to_delete;
 			SET SQL_SAFE_UPDATES = 1;
 			SET is_success = 1;
-			COMMIT;
 	END CASE;
 END$$
 DELIMITER ;
